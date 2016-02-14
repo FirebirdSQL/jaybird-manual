39c697bc83a21cfeae8acc590f3fd14815a9d9bd
Mark Rotteveel <mark@lawinegevaar.nl>
2016-02-14 15:59:01 +0100

    Generate TOC and section numbers

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 rmdir /s /q output
 mkdir output
 mkdir output\images
-pandoc -f markdown -t html5 -o output\jaybird_manual.html -s src\jaybird_manual_metadata.yaml src\jaybird_manual.md
+pandoc -N --toc -f markdown -t html5 -o output\jaybird_manual.html -s src\jaybird_manual_metadata.yaml src\jaybird_manual.md
 copy src\images\* output\images
