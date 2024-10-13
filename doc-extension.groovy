aefcd948dbbc5b4014295bf0172312c0c87bcaa8
Mark Rotteveel <mark@lawinegevaar.nl>
2024-10-13 13:10:14 +0200

    Declare canonical URL

--- doc-extension.groovy
+++ doc-extension.groovy
@@ -1,7 +1,7 @@
 // Preprocessor for since/until roles in pdf backend (in html5 backed this is handled using CSS)
 preprocessor {
     document, reader ->
-        if (document.options['backend'] != 'pdf') {
+        if (!document.basebackend('pdf')) {
             println 'skipping since/until preprocessor for non-PDF'
             return
         }
@@ -26,4 +26,12 @@ preprocessor {
             }
         }
         reader.restoreLines(replacement)
+}
+docinfo_processor {
+    document -> {
+        if (!document.basebackend('html') || !document.hasAttribute('fb-canonical-html')) {
+            return
+        }
+        return "<link rel=\"canonical\" href=\"${document.getAttribute('fb-canonical-html')}\"/>"
+    }
 }
