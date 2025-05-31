ab79874c81b2a7e4a652164ba750c6acb17d4f65
Mark Rotteveel <mark@lawinegevaar.nl>
2025-05-31 14:17:17 +0200

    Check backend instead of basebackend
    It seems backend pdf now also reports basebackend html

--- doc-extension.groovy
+++ doc-extension.groovy
@@ -29,7 +29,7 @@ preprocessor {
 }
 docinfo_processor {
     document -> {
-        if (!document.basebackend('html') || !document.hasAttribute('fb-canonical-html')) {
+        if (document.getAttribute('backend') != 'html5' || !document.hasAttribute('fb-canonical-html')) {
             return
         }
         return "<link rel=\"canonical\" href=\"${document.getAttribute('fb-canonical-html')}\"/>"
